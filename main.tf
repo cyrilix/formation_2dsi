cf439d4d58359b2f55dac27b7c76eef582c0f78d
Cyrille Nofficial <cyrille.nofficial@renault-digital.com>
2017-10-23 14:39:55 +0000

    Add second public subnet

--- main.tf
+++ main.tf
@@ -10,9 +10,18 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_subnet" "subnet_public" {
+resource "aws_subnet" "subnet_public_1" {
   vpc_id     = "${aws_vpc.vpc.id}"
   cidr_block = "172.23.0.0/24"
+
+  tags {
+    Name = "cyrille"
+  }
+}
+
+resource "aws_subnet" "subnet_publici_2" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "172.23.1.0/24"
 
   tags {
     Name = "cyrille"
