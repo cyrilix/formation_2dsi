b83c55081915a996b66af98eb50cf20d4b41293e
Cyrille Nofficial <cyrille.nofficial@renault-digital.com>
2017-10-23 15:30:56 +0000

    Enable route with gateway

--- main.tf
+++ main.tf
@@ -13,13 +13,13 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "subnet_public_1" {
   vpc_id     = "${aws_vpc.vpc.id}"
   cidr_block = "172.23.0.0/24"
-
+ map_public_ip_on_launch = true
   tags {
     Name = "cyrille"
   }
 }
 
-resource "aws_subnet" "subnet_publici_2" {
+resource "aws_subnet" "subnet_public_2" {
   vpc_id     = "${aws_vpc.vpc.id}"
   cidr_block = "172.23.1.0/24"
 
@@ -34,5 +34,35 @@ resource "aws_internet_gateway" "gw" {
   tags {
     Name = "cyrille"
   }
+}
+
+resource aws_eip "eip_nat" {
+}
+
+resource "aws_nat_gateway" "gw_nat" {
+  allocation_id = "${aws_eip.eip_nat.id}"
+  subnet_id     = "${aws_subnet.subnet_public_1.id}"
+
+  tags {
+    Name = "cyrille"
+  }
+}
+
+
+resource "aws_route_table" "route_table1" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "cyrille"
+  }
+}
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.subnet_public_1.id}"
+  route_table_id = "${aws_route_table.route_table1.id}"
 }
 
